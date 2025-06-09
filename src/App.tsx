51f8a552097cdd3d1bfb80517dd9a9e4bfeec097
Bruno De Almeida Otero <pt3025535@IFSPROOT.PTB>
2025-06-09 20:16:22 -0300

    feat: more input customization

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,19 @@ function App() {
 
   return (
     <>
+    <form>
       <CustomInput
         label="Endereço completo"
+        placeholder="Digite seu endereço"
+        onChange={(e) => console.log(e.target.value)}
+        validator={(value) => value.toLowerCase().includes("rua") || value.toLowerCase().includes("avenida") || value.toLowerCase().includes("estrada")}
+        invalidText='⚠ Endereço inválido'
+        hintText='ℹ Deve conter palavra "Rua", "Avenida" ou "Estrada"'
+        isRequired={true}
+        style={{marginBottom: '55px'}}
       />
+      <button type='submit' style={{width: "100%"}}>Enviar</button>
+      </form>
     </>
   )
 }
--- src/components/CustomInput/index.tsx
+++ src/components/CustomInput/index.tsx
@@ -1,33 +1,93 @@
 import { useState } from "react";
 
 interface inputParams {
+    type?: React.HTMLInputTypeAttribute,
+    isRequired?: boolean,
+    isDisabled?: boolean,
     label?: string,
+    placeholder?: string,
+    onChange?: (value: React.ChangeEvent<HTMLInputElement>) => void,
+    validator?: (value: string) => boolean,
+    invalidText?: string,
+    hintText?: string,
+    style?: React.CSSProperties,
+    inputStyle?: React.CSSProperties,
+    labelStyle?: React.CSSProperties,
+    invalidStyle?: React.CSSProperties,
+    hintStyle?: React.CSSProperties
 }
 
-const componentStyle: React.CSSProperties = {
-    position: "relative"
+const defaultStyle: React.CSSProperties = {
+    position: "relative",
 }
 
-const labelStyle: React.CSSProperties = {
+const defaultInputStyle: React.CSSProperties = {
+    padding: "5px"
+}
+
+const defaultLabelStyle: React.CSSProperties = {
     position: "absolute",
+    left: "5px",
+    top: "5px",
     transition: "transform .2s ease"
 }
 
+const defaultInvalidStyle: React.CSSProperties = {
+    position: "absolute",
+    top: '100%',
+    color: 'red',
+    minWidth: 'max-content' 
+}
 
-function CustomInput({label}: inputParams) {
+const defaultHintStyle: React.CSSProperties = {
+    position: "absolute",
+    top: '200%',
+    minWidth: 'max-content' 
+}
+
+function CustomInput({
+    type = "text",
+    isRequired = false,
+    isDisabled = false,
+    label = "",
+    placeholder = "",
+    onChange = () => {},
+    validator = () => true,
+    invalidText = "⚠ Texto inválido",
+    hintText = "ℹ Texto não satisfaz requisitos",
+    style = {},
+    inputStyle = {},
+    labelStyle = {},
+    invalidStyle = {},
+    hintStyle = {},
+}: inputParams) {
 
     const [inputValue, setInputValue] = useState<string>("");
     const [isHovering, setIsHovering] = useState<Boolean>(false);
     const [isSelected, setIsSelected] = useState<Boolean>(false);
 
-    const moveLabelUp = inputValue == "" && !isHovering && !isSelected ? "translateY(0px)" : "translateY(-20px)";
+    const moveLabelUp = inputValue == "" && !isHovering && !isSelected ? "translateY(0px)" : "translateY(-25px)";
+    const showPlaceholder = isHovering || isSelected;
+    const isValid = validator(inputValue);
 
     return (
-        <div style={componentStyle} onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)}>
-            <div style={{...labelStyle, transform: moveLabelUp}}>{label}</div>
-            <input value={inputValue} onChange={(e) => setInputValue(e.target.value)}
+        <div style={{...defaultStyle, ...style}} onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)}>
+            <div style={{...defaultLabelStyle, transform: moveLabelUp, ...labelStyle}}>{label}</div>
+            <input 
+                style={{...defaultInputStyle, ...inputStyle}}
+                type={type}
+                value={inputValue} 
+                onChange={(e) => {
+                    onChange(e);
+                    setInputValue(e.target.value);
+                }}
                 onFocus={() => setIsSelected(true)} onBlur={() => setIsSelected(false)}
+                placeholder={showPlaceholder ? placeholder : ''}
+                required={isRequired}
+                disabled={isDisabled}
             />
+            <div style={{...defaultInvalidStyle, ...invalidStyle}}>{!isValid && inputValue && !isSelected ? invalidText : ""}</div>
+            <div style={{...defaultHintStyle, ...hintStyle}}>{!isValid && inputValue && (isSelected || isHovering) ? hintText : ""}</div>
         </div>
     )
 }
